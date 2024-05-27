80632e0766db1f841509d93eea2951a4928d60e2
Ferre <ferremolenbeek@gmail.com>
2024-05-27 19:27:25 +0200

    added full string check if n = 0

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ferre <ferre@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/04 13:57:05 by fmolenbe      #+#    #+#                 */
-/*   Updated: 2024/05/26 17:25:22 by ferre         ########   odam.nl         */
+/*   Updated: 2024/05/27 19:26:15 by ferre         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (i < n)
+	while (i < n || n == 0)
 	{
 		if ((unsigned char)s1[i] != (unsigned char)s2[i])
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
