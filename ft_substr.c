dfb83ffd907468acc75fc6b1089b2e9acad3bea9
Ferre <ferremolenbeek@gmail.com>
2024-05-27 21:55:50 +0200

    Rewrote cause old code was shit

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ferre <ferre@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/12 15:52:39 by fmolenbe      #+#    #+#                 */
-/*   Updated: 2024/05/26 17:25:22 by ferre         ########   odam.nl         */
+/*   Updated: 2024/05/27 21:55:06 by ferre         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,22 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*p;
-	char	a[100000];
-	size_t	i;
+	char	*sub;
+	int		i;
 
-	i = -1;
-	start--;
-	while (ft_strlen(s) > ++start && ((i + 1) < len) && s[start])
-		a[++i] = s[start];
-	a[++i] = '\0';
-	p = ft_strdup((char *)a);
-	return (p);
+	if (start >= ft_strlen(s))
+		return (NULL);
+	if ((start + (unsigned int)len) > ft_strlen(s))
+		len = ft_strlen(s) - start;
+	sub = malloc(sizeof(char) * (len + 1));
+	if (!sub)
+		return (NULL);
+	i = 0;
+	while (i < len && s[start + i])
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
 }
