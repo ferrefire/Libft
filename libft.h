42ce07ebbd65f1af7d8cea8e5e05dbda07359ea3
ferre <ferremolenbeek@gmail.com>
2024-06-03 18:49:54 +0200

    removed unused function from header file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ferre <ferre@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/06 15:18:34 by fmolenbe      #+#    #+#                 */
-/*   Updated: 2024/05/26 17:25:22 by ferre         ########   odam.nl         */
+/*   Updated: 2024/06/03 18:49:21 by ferrefire     ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,6 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
-char	*ft_strcat(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s1, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
